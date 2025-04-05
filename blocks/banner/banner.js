013751f7a5c0654b97c17b689a2f61a510cb16d0
RDevRoy <160475108+RDevRoy@users.noreply.github.com>
2025-04-05 21:46:26 +0530

    updating the banner.js

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -7,10 +7,8 @@ export default function decorate(block) {
   hero.innerHTML = `
       <div class="max-w-7xl mx-auto px-4 py-20">
         <div class="mb-4 inline-flex items-center space-x-2 text-sm font-medium text-gray-400">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-          </svg>
-          <span>Do you know what awesomeness is ??</span>
+         <img src="https://www.svgrepo.com/show/303503/shopify-logo.svg" alt="Shopify logo" class="h-5 w-auto">
+          <span>Do you know what shopify awesomeness is ??</span>
         </div>
         <h1 class="text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl">Welcome to ShopiFloyd Store</h1>
         <p class="mt-4 text-lg text-gray-400 max-w-2xl">
